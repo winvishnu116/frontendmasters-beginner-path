f223a460ecd9b2fb9d2a88cac12963f53579a5f8
winvishnu116 <winvishnu116@gmail.com>
2024-07-23 22:24:42 +0530

    Wordle Clone - intro commit
    To maintain streak

---
+++ HTML, CSS and JS/Project 4 - Wordle Clone/wordleclone.js
@@ -0,0 +1,4 @@
+// need to fetch word of the day on initialization
+// on keyup of each character need to store the character in memory and display it on the screen if it is valid
+// once enter key is pressed need to validate the input string only if there are five characters.
+// once validation is done need to display the result on the screen and highlight the UI
